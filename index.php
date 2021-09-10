ac6ea95aae0d53414f2e4fa6489f912494859b39
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-09-10 22:28:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 										   
 <body>
    
-<h1>62101456 Jirameth Yukachain</h1>
+<h1>62101456 จิรเมธ อยู่คะเชนทร์ </h1>
  <?php
 $api_url = 'https://api.thingspeak.com/channels/1502788/feeds.json?api_key=UMT1JOXQHK75PNRS&results=1';
 $json_data = file_get_contents($api_url);
@@ -38,15 +38,7 @@ width="500" frameBorder="0" title="Iframe Example"></iframe>
 	echo "Light: ".$user->field3; 
       echo " FC ";
    }?> 
- <br>
- <iframe src="https://thingspeak.com/channels/1502788/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"height="250"
-width="500" frameBorder="0" title="Iframe Example"></iframe>
-<?php
-   foreach ($user_data as $user) {
-	echo "i: ".$user->field4; 
-      echo " FC ";
-   }?> 
- <br>                                                    
+ <br>                                       
 <iframe src="https://thingspeak.com/channels/1502788/maps/channel_show" height="250" width="500" frameBorder="0" title="Iframe Example"></iframe>
 <br>         
  </body>
